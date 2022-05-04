8b29d7d552db754e18f5a84964e70faa80513596
rockstarcubalife <104049632+rockstarcubalife@users.noreply.github.com>
2022-05-03 22:37:21 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "213e85670cd03dfdcfc4936c86d153a2"
-api_id = 15091118
-bot_token = "5222714351:AAGJIy9EWsfa6HEZtsDL9IU7HxeCaGYqm1o"
-bot_admin_group = -1001766577678
+api_hash = "0c780f6db3124fafbdc3dcde437e6f21
+api_id = 16536294
+bot_token = "5187182672:AAF8Emxj0Y7XnwHlNYTOW-tt-WYVzmTlkbg"
+bot_admin_group = -1001505005021
